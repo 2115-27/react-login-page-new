f5fabe31bc275def3e7c8a48cb85367f8f5dce55
Malavika1113 <74175454+Malavika1113@users.noreply.github.com>
2023-01-02 13:36:06 +0530

    Update HandleLogin.jsx

--- src/components/HandleLogin.jsx
+++ src/components/HandleLogin.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import './App1.css';
 
 const Home = () => {
+  const handleClick = async () => {
+	  console.log("handleclick")
+	  const octokit = new Octokit({
+  		auth: process.env.TOKEN,
+	})
+
+  await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+    owner: '2115-27',
+    repo: 'react-login-page',
+    workflow_id: 'Reuseable-A.yml',
+    ref: 'main'
+    })
+  };
   return (
     <div className="App">
       <header className="App-header">
