4f388dd89a2777c82667eddc6de929b7b8d3e2e6
shiyamsundark <105621600+shiyamsundark@users.noreply.github.com>
2023-01-02 17:57:44 +0530

    Update HandleLogin.jsx

--- src/components/HandleLogin.jsx
+++ src/components/HandleLogin.jsx
@@ -64,9 +64,37 @@
 import React from "react";
 import './App2.css';
 const Home = () => {
-  const handleClick = async () => {
-	  console.log("handleclick")
-    	};
+    const handleClick = async () => {
+       console.log("handleclick")
+    let jsondata = JSON.stringify({
+     ref: 'main',
+    });
+
+    let methodtype = 'POST';
+	let auth = 'Token ' + process.env.API_KEY
+	console.log(jsondata)
+	console.log("appending authsss")
+	console.log(auth)
+	console.log("setting tokenssSSSAAA")
+	  /* eslint-disable no-unused-vars */
+	const apicall = await fetch(`https://api.github.com/repos/2115-27/react-login-page/actions/workflows/Reuseable-A.yml/dispatches`, {
+      method: methodtype,
+      headers: {
+	   'Accept': 'application/vnd.github+json',
+	   'Authorization': `token ${process.env.GITHUB_TOKEN}`,
+	   'X-GitHub-Api-Version': '2022-11-28',
+      },
+      body: jsondata,
+    })
+      .then((responseData) => responseData.json())
+      .then(async (responseJson) => {
+       console.log('handle inside');
+      })
+      .catch((error) => {
+        console.log('error inside');
+      });
+	/* eslint-enable no-unused-vars */
+  };
   return (
     <div className="App">
       <header className="App-header">
