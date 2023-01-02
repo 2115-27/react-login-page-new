f4f68f29c6af2e477e5353904021dbdc2a25b564
shiyamsundark <105621600+shiyamsundark@users.noreply.github.com>
2023-01-02 13:10:43 +0530

    Update HandleLogin.jsx

--- src/components/HandleLogin.jsx
+++ src/components/HandleLogin.jsx
@@ -12,11 +12,11 @@ const Home = () => {
          <div className="input-container">
            <label>Release Version  </label>
            <input type="text" name="uname" required />
-         </div><br>
+         </div><br/>
          <div className="input-container">
            <label>Model Type  </label>
            <input type="text" name="pass" required />
-         </div><br>
+         </div><br/>
          <button type="button" class="build-button">BUILD</button>
         </form>
       </header>
